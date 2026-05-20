6e86cd67bf6e4a748acdb53c10ea635c0e629d3b
Asal himsanda <150653318+asalhanadapangoda@users.noreply.github.com>
2026-05-21 00:04:18 +0530

    Complete Backend Create Main function

---
+++ src/main/java/org/example/SbadssBackendApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbadssBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SbadssBackendApplication.class, args);
+    }
+}
